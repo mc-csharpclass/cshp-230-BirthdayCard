191a3cd60b6037cc95403c82c0034180eaf7faa3
mc-csharpclass <36938152+mc-csharpclass@users.noreply.github.com>
2018-04-17 04:18:49 +0900

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BirthdayCard.Models;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult HappyBirthday()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public ActionResult Index(Models.BirthdayInfo birthdayinfo)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("HappyBirthday", birthdayinfo);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/BirthdayInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCard.Models
+{
+    public class BirthdayInfo
+    {
+        [Required(ErrorMessage = "Please enter you name")]
+        public string SenderName { get; set; }
+        [Required(ErrorMessage = "Please enter the birthday person's name")]
+        public string RecieverName { get; set; }
+        [Required(ErrorMessage = "Please enter a birthday message")]
+        public string Message { get; set; }
+    }
+}
